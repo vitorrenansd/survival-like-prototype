68320271e2abf21247a25783b6109380d4c358dc
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-11-28 19:46:27 -0300

    Criado a classe com os modificadores de stats

---
+++ scripts/classes/StatsModifier.gd
@@ -0,0 +1,8 @@
+class_name StatsModifier
+extends Resource
+
+@export var bonus_health: int = 0
+
+@export var damage_multiplier: float = 1.0
+@export var attack_speed_multiplier: float = 1.0
+@export var move_speed_multiplier: float = 1.0
