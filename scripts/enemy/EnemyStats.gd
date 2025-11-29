1d794d900cc42afd011f248c4a699f2d0395f414
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-11-29 14:22:29 -0300

    Criados os atributos de ATK SPD e MS fixos para classe Enemy

--- scripts/enemy/EnemyStats.gd
+++ scripts/enemy/EnemyStats.gd
@@ -2,10 +2,12 @@ class_name EnemyStats
 extends Node
 
 # Por enquanto mexer apenas na vida e dano, senão fica muito OP
-# Trabalhar na quantidade, e não no movespeed impossível ou algo assim
+# Trabalhar na quantidade de inimigos, e não no movespeed impossível ou algo assim
 
 @export var total_health: int
 @export var total_damage: float
+@export var total_attack_speed: float = 0.75
+@export var total_move_speed: float = 200.0
 var enemy: Enemy
 
 
