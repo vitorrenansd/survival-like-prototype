d7c0bd6bb187ba7400865d3a5d9f4596a87e1322
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-11-29 00:07:15 -0300

    Removido o prefixo 'base' para evitar confusão

--- scripts/classes/WarriorBaseStats.gd
+++ scripts/classes/WarriorBaseStats.gd
@@ -2,8 +2,7 @@ class_name WarriorBaseStats
 extends Resource
 
 ## Futuramente podem ser criadas outras classes para outros bonecos (mago, tanker, etc)
-@export var base_health: int = 100
-
-@export var base_damage: float = 10.0
-@export var base_attack_speed: float = 1.0
-@export var base_move_speed: float = 200.0
+@export var health: int = 100
+@export var damage: float = 10.0
+@export var attack_speed: float = 1.0
+@export var move_speed: float = 200.0
