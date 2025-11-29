68e75d52f1e00e9aad4488cfa0f68412ccabcb0b
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-11-29 00:06:11 -0300

    Adicionado campo String para nome do item

--- scripts/classes/StatsModifier.gd
+++ scripts/classes/StatsModifier.gd
@@ -1,8 +1,9 @@
 class_name StatsModifier
 extends Resource
 
+## Modificador de stats, classe do item/livro/buff do jogo
+@export var name: String = ""
 @export var bonus_health: int = 0
-
 @export var damage_multiplier: float = 1.0
 @export var attack_speed_multiplier: float = 1.0
 @export var move_speed_multiplier: float = 1.0
