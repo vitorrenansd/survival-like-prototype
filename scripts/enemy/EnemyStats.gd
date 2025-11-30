f9f39f9bba5c972c78359032db99339b5fea7652
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-11-30 00:06:52 -0300

    Retirado prefixo desnecessario 'total' nas var de Stats

--- scripts/enemy/EnemyStats.gd
+++ scripts/enemy/EnemyStats.gd
@@ -2,10 +2,11 @@ class_name EnemyStats
 extends Node
 
 # Por enquanto não mexer no atkspd, senão fica muito OP
-@export var total_health: int
-@export var total_damage: float
-@export var total_attack_speed: float = 0.75
-@export var total_move_speed: float
+@export var health: float
+@export var current_health: float
+@export var damage: float
+@export var attack_speed: float = 1.0
+@export var move_speed: float
 var enemy: Enemy
 
 
@@ -14,8 +15,8 @@ func _init(e): # Construtor da classe: necessita Enemy
 
 func recalculate_stats() -> void:
 	# Primeiro volta aos valores base, dps aplica o bonus da wave atual
-	total_health = enemy.base_stats.health
-	total_damage = enemy.base_stats.damage
-	total_move_speed = enemy.base_stats.move_speed
+	health = enemy.base_stats.health
+	damage = enemy.base_stats.damage
+	move_speed = enemy.base_stats.move_speed
 	
 	## FALTANDO O CALCULO PELO NUMERO DA WAVE
--- scripts/player/PlayerStats.gd
+++ scripts/player/PlayerStats.gd
@@ -1,10 +1,12 @@
 class_name PlayerStats
 extends Node
 
-@export var total_health: int
-@export var total_damage: float
-@export var total_attack_speed: float
-@export var total_move_speed: float
+@export var health: float
+@export var current_health: float
+@export var damage: float
+@export var attack_speed: float
+@export var move_speed: float
+@export var health_regen: int
 var player: Player
 
 
@@ -13,13 +15,14 @@ func _init(p): # Construtor da classe: necessita Player
 
 func recalculate_stats() -> void:
 	# Primeiro volta aos valores base, dps aplica todos os modificadores ativos
-	total_health = player.base_stats.health
-	total_damage = player.base_stats.damage
-	total_attack_speed = player.base_stats.attack_speed
-	total_move_speed = player.base_stats.move_speed
+	health = player.base_stats.health
+	damage = player.base_stats.damage
+	attack_speed = player.base_stats.attack_speed
+	move_speed = player.base_stats.move_speed
+	health_regen = player.base_stats.health_regen
 	
 	for m in player.modifiers:
-		total_health += m.bonus_health
-		total_damage *= m.damage_multiplier
-		total_attack_speed *= m.attack_speed_multiplier
-		total_move_speed *= m.move_speed_multiplier
+		health += m.bonus_health
+		damage *= m.damage_multiplier
+		attack_speed *= m.attack_speed_multiplier
+		move_speed *= m.move_speed_multiplier
