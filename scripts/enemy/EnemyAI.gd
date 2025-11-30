e700df5021383a26f676a97a798cbe3f98700cee
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-11-30 00:04:53 -0300

    Criado o metodo chase_player() na classe EnemyAI

--- scripts/enemy/EnemyAI.gd
+++ scripts/enemy/EnemyAI.gd
@@ -2,7 +2,17 @@ class_name EnemyAI
 extends Node
 
 var enemy: Enemy
+var player: Player
 
 
 func _init(e): # Construtor da classe: necessita Enemy
 	enemy = e
+
+func _physics_process(_delta):
+	chase_player()
+
+func chase_player(): # Persegue o jogador
+	var enemy_ms = enemy.current_stats.move_speed
+	var direction = (player.global_position - enemy.global_position).normalized()
+	enemy.velocity = direction * enemy_ms
+	enemy.move_and_slide()
