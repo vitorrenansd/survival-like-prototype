13b1a23f73caf6f9d8538c6286d2ca1a4c714e05
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-11-30 00:03:58 -0300

    Criados os metodos basicos attack() e take_damage()

--- scripts/enemy/Enemy.gd
+++ scripts/enemy/Enemy.gd
@@ -1,13 +1,27 @@
 class_name Enemy
 extends CharacterBody2D
 
-@export var base_stats: Resource # Recebe uma classe de inimigo (evil skull, ...)
+@export var base_stats: Resource # Recebe um tipo de inimigo (evil skull, ...)
 @export var current_stats: EnemyStats
-@export var current_health: int
 var enemy_ai: EnemyAI
-
+var can_attack := true
 
 func _ready():
 	current_stats = EnemyStats.new(self)
 	enemy_ai = EnemyAI.new(self)
 	current_stats.recalculate_stats()
+
+func attack(player: Player):
+	if not can_attack:
+		return
+	
+	can_attack = false
+	player.take_damage(current_stats.damage)
+	await get_tree().create_timer(current_stats.attack_speed).timeout
+	can_attack = true
+
+func take_damage(amount: float):
+	current_stats.health -= amount
+	current_stats.health = floor(current_stats.health) # Arredonda pra baixo
+	if current_stats.health <= 0.0:
+		queue_free() # morre/exclui
