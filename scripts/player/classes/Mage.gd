c8d5dc51cc1d49e123f69ca9e3ea61879cd56f7e
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-11-30 00:02:56 -0300

    Adicionado o atributo health_regen para as classes de personagem

--- scripts/player/classes/Mage.gd
+++ scripts/player/classes/Mage.gd
@@ -5,3 +5,4 @@ var health: int = 65
 var damage: float = 14.0
 var attack_speed: float = 1.0
 var move_speed: float = 320.0
+var health_regen: int = 1
--- scripts/player/classes/Warrior.gd
+++ scripts/player/classes/Warrior.gd
@@ -5,3 +5,4 @@ var health: int = 100
 var damage: float = 10.0
 var attack_speed: float = 1.0
 var move_speed: float = 250.0
+var health_regen: int = 2
