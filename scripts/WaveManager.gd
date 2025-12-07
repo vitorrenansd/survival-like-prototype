0ef6550ddeeea679c248d3d1d70b77ef013025e4
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-12-07 10:26:31 -0300

    Sincronizando os commits feitos no outro PC

--- scripts/WaveManager.gd
+++ scripts/WaveManager.gd
@@ -15,7 +15,7 @@ func _ready():
 ## esse metodo ta feito com a bunda!!!!
 ## corrigir para depois
 func spawn_enemies():
-	var evil_skull = preload("res://characters/enemies/evil_skull.tscn").instantiate()
+	var evil_skull = preload("res://scenes/characters/enemies/evil_skull.tscn").instantiate()
 	%EnemySpawner.progress_ratio = randf() # Escolhe um lugar random da linha de Spawner
 	evil_skull.global_position = %EnemySpawner.global_position # Coloca o inimigo la
 	add_child(evil_skull)
--- scripts/Weapon.gd
+++ scripts/Weapon.gd
@@ -4,7 +4,7 @@ extends Area2D
 const BULLET = preload("res://assets/weapons/bullet.tscn")
 
 
-func _physics_process(_delta): ## Função que locka no inimigo
+func _physics_process(_delta): ## Função que locka no inimigo mais perto
 	var enemies_in_range = get_overlapping_bodies()
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range[0]
