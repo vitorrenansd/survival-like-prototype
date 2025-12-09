0d2f16714699e962aa80fde76c65e7df9705c100
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-12-09 15:12:16 -0300

    update: Nerf no move_speed do EvilSkull (250 para 200)

--- scripts/enemy/classes/EvilSkull.gd
+++ scripts/enemy/classes/EvilSkull.gd
@@ -4,4 +4,4 @@ extends Resource
 ## Futuramente podem ser criadas outras classes para outros inimigos
 var health: int = 10
 var damage: float = 10.0
-var move_speed: float = 250
+var move_speed: float = 200
