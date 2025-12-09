801242b8a8daae122104b845d8501c908b441a22
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-12-09 15:20:04 -0300

    refactor: arma não atira mais infinitamente, primeiro ve se tem inimigo

--- scripts/Weapon.gd
+++ scripts/Weapon.gd
@@ -8,24 +8,27 @@ var current_stats: PlayerStats
 
 
 func setup(ps: PlayerStats):
-	current_stats = ps
-	shoot_timer.wait_time = 1.0 / current_stats.attack_speed
-	shoot_timer.start()
+	self.current_stats = ps
+	self.shoot_timer.wait_time = 1.0 / self.current_stats.attack_speed
+	self.shoot_timer.one_shot = true
+	self.shoot_timer.autostart = false 
 
 func _physics_process(_delta):
-	lock_at_nearest()
+	lock_n_load()
 
-func _on_shoot_timer_timeout():
-	fire()
+func ready_to_shoot() -> bool:
+	return shoot_timer.is_stopped()
 
-func lock_at_nearest():  ## Função que locka no inimigo mais proximo
+func lock_n_load() -> void:  ## Função que locka e atira no inimigo mais proximo
 	var enemies_in_range = get_overlapping_bodies()
-	if enemies_in_range.size() > 0:
-		var target_enemy = enemies_in_range[0]
-		look_at(target_enemy.global_position)
+	if enemies_in_range.size() > 0 and ready_to_shoot():
+		var nearest_enemy = enemies_in_range[0]
+		look_at(nearest_enemy.global_position)
+		fire()
 
 func fire():
 	var new_bullet = BULLET.instantiate()
 	new_bullet.global_position = shooting_point.global_position
 	new_bullet.global_rotation = shooting_point.global_rotation
 	get_tree().current_scene.add_child(new_bullet)
+	shoot_timer.start() ## Reseta cooldown
